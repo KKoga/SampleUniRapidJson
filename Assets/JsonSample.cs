3bb1f40ac6d4a041cd39442e8276f660d495888f
古賀 健太 <kenta.koga@IT-04176-M.local>
2019-04-09 16:06:36 +0900

    first commit

---
+++ Assets/JsonSample.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class JsonSample : MonoBehaviour
+{
+    public Text CurrentStatus;
+    public Text ResultOfParse;
+    public Button Trigger;
+
+    bool running;
+    int updated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updated = 0;
+        running = true;
+        OnClick();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+            executeParser();
+        }
+    }
+
+    public void OnClick()
+    {
+        running = !running;
+        Trigger.GetComponentInChildren<UnityEngine.UI.Text>().text = running ? "stop" : "start";
+    }
+
+    // create json text with current datetime text and parse it.
+    void executeParser()
+    {
+        var jsonSource = $@"{{
+            ""number"": 123.456,
+            ""bool"": true,
+            ""string"": ""C/C++ C# Unity"",
+            ""array"": [1,2,3,4,5,6,7],
+            ""object"": {{ ""1"": 20 }},
+            ""datetime"": ""{System.DateTime.Now.ToString()}""
+        }}";
+        for (var i = 0; i < 1000; i++)
+        {
+            var rr = rapidjson.Document.Parse(jsonSource);
+        }
+        var r = rapidjson.Document.Parse(jsonSource);
+        var now = r.Root["datetime"].ToString();
+        ResultOfParse.text = now;
+        CurrentStatus.text = updated.ToString();
+        updated++;
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+### SampleUniRapidJson
+
+  This sample uses UniRapidJson repeatedly. The script file is Assets/JsonSample.cs.
+
+### How To Update UniRapidJson
+
+  * Download UniRapidJson and build for target platforms.
+  * Execute generate_unitypackage.py that generates unitypackage. It requires python3.
+  * Pickup UniRapidJson.unitypackage from dist/ folder.
+  * Import UnityPackage to SampleUniRapidJson/Assets/exlibs.
+
+### Tips
+  * RapidJson has Generator/Parser functions.
+  * UniRapidJson has only Parser function.
+
